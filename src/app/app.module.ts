2149205316e8e8882a2f85f253a955539e3c8334
Shashi Kumar <shashikumar@SBMAC0389.local>
2020-01-29 17:16:36 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DoubleComponent } from './double/double.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoubleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/double/double.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-double',
+  templateUrl: './double.component.html',
+  styleUrls: ['./double.component.css']
+})
+export class DoubleComponent implements OnInit {
+
+  constructor() { }
+  activeClass=false;
+
+  ngOnInit() {
+  }
+  toggleClass(){
+    this.activeClass=!this.activeClass
+  }
+
+}
